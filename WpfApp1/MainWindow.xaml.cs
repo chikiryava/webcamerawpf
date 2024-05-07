ebfad5298b7894d3fc778aea4f71f6359ccadb0a
chikiryava <chikiryava@gmail.com>
2024-05-08 00:11:17 +0500

    first commit

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private FilterInfoCollection _videoDevices;
+        private VideoCaptureDevice _videoSource;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            _videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (_videoDevices.Count <= 0)
+            {
+                MessageBox.Show("Видеоустроства не были найдены");
+                Close();
+                return;
+            }
+
+            _videoSource = new VideoCaptureDevice(_videoDevices[0].MonikerString);
+            _videoSource.NewFrame += VideoSource_NewFrame;
+            _videoSource.Start();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (_videoSource != null && _videoSource.IsRunning)
+            {
+                _videoSource.SignalToStop();
+            }
+        }
+
+        private void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            using (var bitmap = (Bitmap)eventArgs.Frame.Clone())
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    var bitmapSource = ConvertBitmapToBitmapSource(bitmap);
+                    WebcamImage.Source = bitmapSource;
+                });
+            }
+        }
+
+        private BitmapSource ConvertBitmapToBitmapSource(Bitmap bitmap)
+        {
+            var bitmapData = bitmap.LockBits(
+                new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                ImageLockMode.ReadOnly, bitmap.PixelFormat);
+
+            var bitmapSource = BitmapSource.Create(
+                bitmapData.Width, bitmapData.Height,
+                bitmap.HorizontalResolution, bitmap.VerticalResolution,
+                PixelFormats.Bgr24, null,
+                bitmapData.Scan0, bitmapData.Stride * bitmapData.Height, bitmapData.Stride);
+
+            bitmap.UnlockBits(bitmapData);
+            return bitmapSource;
+        }
+    }
+}
